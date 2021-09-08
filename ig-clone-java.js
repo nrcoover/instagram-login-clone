3c34fc4612da22c315a5ee662ecbf5b90a6bdf6d
nrcoover <81087345+nrcoover@users.noreply.github.com>
2021-09-08 12:16:09 -0400

    Login Button Functions Properly
    Completed the JavaScript code necessary to make the Login Button function as intended. Was previously missing the necessary code in the html to have it respond on key up. That has been amended.

--- ig-clone-java.js
+++ ig-clone-java.js
@@ -1,18 +1,4 @@
-// document.getElementById("login-btn").disabled=true;
-
-
-// login-btn.setAttribute('disabled', 'disabled');
-
-
-// document.getElementbyId("login-btn").addEventListener('form-input', function() {
-//   if (document.getElementById("username").value.length > 0 && document.getElementById("password").value.length > 5) {
-//       document.getElementById("login-btn").disabled=false;
-//     } else {
-//       document.getElementById("login-btn").disabled=true;
-//     }
-// });
-
-
+// LOGIN BUTTON ENABLE + DISABLE FUNCTION
 
 let username = document.querySelector("#username");
 let password = document.querySelector("#password")
@@ -23,10 +9,9 @@ username.addEventListener("change", stateHandle);
 password.addEventListener("change", stateHandle);
 
 function stateHandle() {
-  if (username.value > 0 && password.value > 5 ) {
-    loginButton.disabled = true; 
+  if (username.value.length > 0 && password.value.length > 5 ) {
+    loginButton.disabled = false; 
   } else {
-    loginButton.disabled = false;
+    loginButton.disabled = true;
   }
 }
-
