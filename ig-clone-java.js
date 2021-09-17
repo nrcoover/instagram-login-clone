3057d35e7450a377f850eaa3c5d427b1970cfeca
nrcoover <81087345+nrcoover@users.noreply.github.com>
2021-09-16 22:13:48 -0400

    Added Show/Hide Buttons to Password
    Added HTML, CSS, and JavaScript to have Show/Hide buttons function within Password Input. Originally had Show/Hide visibility changed in JavaScript (the commented out function), but later added that via the CSS rule for "input:not(placeholder-shown) ~ #show-btn"

--- ig-clone-java.js
+++ ig-clone-java.js
@@ -3,15 +3,48 @@
 let username = document.querySelector("#username");
 let password = document.querySelector("#password")
 let loginButton = document.querySelector("#login-btn");
+let showButton = document.querySelector("#show-btn");
+let hideButton = document.querySelector("#hide-btn");
 
 loginButton.disabled = true;
-username.addEventListener("change", stateHandle);
-password.addEventListener("change", stateHandle);
+// username.addEventListener("change", stateHandle);
+// password.addEventListener("change", stateHandle);
 
 function stateHandle() {
-  if (username.value.length > 0 && password.value.length > 5 ) {
+  if (username.value.length > 0 && password.value.length > 4 ) {
     loginButton.disabled = false; 
   } else {
     loginButton.disabled = true;
   }
 }
+
+function stateHandleUp() {
+  if (password.value.length > 5 ) {
+    loginButton.dsabled = false;
+  } else {
+    loginButton.disabled = true;
+  }
+}
+
+function showPassword() {
+  if (password.type === "password") {
+    password.type = "text";
+    showButton.style.display = "none";
+    hideButton.style.visibility = "visible";
+  } else {
+    password.type = "password";
+    hideButton.style.visibility = "hidden";
+    showButton.style.display = "inline-block";
+  }
+}
+
+hideButton.style.visibility = "hidden";
+
+
+// function revealButton() {
+//   if (password.value.length > 0) {
+//     showButton.style.visibility = "visible";
+//   } else {
+//   showButton.style.visibility = "hidden";
+//   }
+// }
